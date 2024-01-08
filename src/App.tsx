948c4143413a2d4c856dbd35290169102804f216
simdaa <simdaa>
2024-01-08 10:37:40 +0900

    Feat: 마우스 좌표 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.scss'
 import CustomInput from './components/CustomInput'
+import { MoveMouse } from './composables/MoveMouse'
+
+function XYLocation() {
+  const { xy, handleMouseMove } = MoveMouse()
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove)
+  }, [handleMouseMove])
+
+  return (
+    <div>
+      <p>X: {xy.x}</p>
+      <p>Y: {xy.y}</p>
+    </div>
+  )
+}
 
 function App() {
   const [inputValue, setInputValue] = useState<string | number>('')
@@ -8,6 +24,7 @@ function App() {
   const handleInputValueChange = (value: string | number) => {
     setInputValue(value)
   }
+
   return (
     <>
       <div className="flex flex-col items-start">
@@ -18,6 +35,10 @@ function App() {
         <p className="flex">
           type: {inputValue ? <p>{typeof inputValue}</p> : ''}
         </p>
+      </div>
+
+      <div className="mt-10">
+        <XYLocation />
       </div>
     </>
   )
